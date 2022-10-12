d200e7c174a532d923da93614fe299729825bde3
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2022-10-11 21:09:08 -0400

    Étapes 2.b : début (sans les variables)

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Laboratoire du cours MGL869 de l'ÉTS
 # Prérequis
 python3
 git
-gitpython
+gitpython (https://pypi.org/project/GitPython/)
+understand (https://licensing.scitools.com/download)
--- collector.py
+++ collector.py
@@ -1,6 +1,7 @@
 import git
 import os
 import re
+import shutil
 import urllib.request as req
 from xml.etree.ElementTree import parse
 
@@ -19,7 +20,12 @@ class Collector:
     git_remote_repo_path = 'https://github.com/apache/' + git_repo_name
     git_local_repo_folder = '../'
     git_local_repo_path = git_local_repo_folder + git_repo_name
-
+    # Path to CSV file listing files per bug
+    bug_files_path = './bug_files.csv'
+    # Path to CSV file listing independent variables per file's versions
+    files_vars_path = './files_vars.csv'
+    # Path to Understand project folder
+    und_project = './und_project'
 
     def __init__(self, bugs_to_collect, vars_to_collect):
         self.bugs_to_collect = bugs_to_collect
@@ -32,13 +38,14 @@ class Collector:
 
         if self.bugs_to_collect:
             self.collect_bugs()
+        if self.vars_to_collect:
+            self.collect_vars()
 
 
     def collect_bugs(self):
         bugs = [] # List of bugs
-        # Output CSV file in format bug_id;file_path
-        output_file = open('./bug_files.csv', 'w')
-        output_file.write('Bogue,Fichier\n')
+        output_file = open(self.bug_files_path, 'w')
+        output_file.write('BugId,Fichier\n')
 
         # Fetch bugs from JIRA
         url = req.urlopen(self.bugs_report_url)
@@ -49,6 +56,7 @@ class Collector:
         
         # Fetch files from Git
         if not os.path.exists(self.git_local_repo_path):
+            # Cloning HIVE repository takes a couple of minutes
             git.Git(self.git_local_repo_folder).clone(self.git_remote_repo_path)
         else:
             git.Repo(self.git_local_repo_path).remotes.origin.pull()
@@ -65,7 +73,69 @@ class Collector:
                 file_ext = os.path.splitext(file)[-1].lower()
                 if file_ext not in ['.cpp', '.java']: # Only consider C++ and Java files
                     continue
-
+                
+                # Print results in CSV output file
                 output_file.write(bug_id + ',' + file + '\n')
 
+        output_file.close() # Save output file
+
+    
+    def collect_vars(self):
+        if not (os.path.exists(self.bug_files_path) and \
+                os.path.exists(self.git_local_repo_path)):
+            return
+
+        output_file = open(self.files_vars_path, 'w')
+        output_file.write('Version,CommitId,Fichier\n')
+
+        # Initialize Understand project's folder
+        if os.path.exists(self.und_project):
+            shutil.rmtree(self.und_project)
+        os.mkdir(self.und_project)    
+
+        # Fetch files from input file
+        files = set() # Set of files
+        # Input CSV file in format bug_id;file_path
+        input_file = open(self.bug_files_path, 'r')
+        for line in input_file.read().splitlines()[1:]:
+            line_values = line.split(',')
+            if len(line_values) <= 1:
+                continue
+
+            files.add(line_values[-1]) # Add file to files set
+        
+        # Fetch files' versions from Git
+        git_repo = git.Repo(self.git_local_repo_path)
+        git_repo.git.reset('--hard') # Reset local repo
+
+        for branch in git_repo.remote().refs:
+            branch_name = branch.name
+            # Ignore branches not mentioning 'branch' nor 'release'
+            if 'branch' not in branch_name and 'release' not in branch_name:
+                continue
+
+            branch_version = re.search('[0-9]+\.[0-9]+\.[0-9]*', branch_name)
+            if not branch_version:
+                continue
+
+            version = branch_version.group() # HIVE's version related to branch
+            git_repo.git.checkout(branch_name) # Restore local repo to given branch
+
+            for f in files:
+                file_commits = list(git_repo.iter_commits(paths = f)) # File's commits in branch
+                if not file_commits: # No commit for file
+                    continue
+
+                file_last_commit = sorted(file_commits, key=lambda c: c.committed_datetime)[-1]
+                file_name = os.path.basename(f)
+
+                # Add file's version content as code file in Understand project
+                code_file = open(self.und_project + '/' + version + '_' + file_name, 'wb')
+                code_file.write((file_last_commit.tree / f).data_stream.read())
+                code_file.close()
+
+                # Print results in CSV output file
+                output_file.write(version + ',' + file_last_commit.hexsha + ',' + f + '\n')
+
+        git_repo.git.reset('--hard') # Reset local repo
         output_file.close() # Save output file
