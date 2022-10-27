7530b8fc6d0f4cb8285f069fce2f63237c986849
aracanelli <ant.racanelli@gmail.com>
2022-10-26 20:55:39 -0400

    Classe ajoutée pour nettoyer les données

---
+++ dataClean.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+class DataClean:
+    clean_file = False
+
+    def __init__(self, clean_file):
+        self.clean_file = clean_file
+
+    def clean_data(self, file_to_clean):
+        if not self.clean_file:
+            return
+
+        files_vars = pd.read_csv(file_to_clean)
+        removed_duplicates = files_vars.drop_duplicates()
+        removed_duplicates.to_csv('files_vars_no_dupes.csv', index=False)
+
+        cleaned_date = removed_duplicates.groupby(["Version", "CommitId", "Fichier", "ContientBogue"], as_index=False).first()
+        cleaned_date.to_csv('cleaned_files_vars.csv', index=False)
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from collector import Collector
+from dataClean import DataClean
 import sys
 
 
 class Main:
     collector = None
+    dataClean = None
 
     def init_collector(self, bugs_to_collect, vars_to_collect):
         self.collector = Collector(bugs_to_collect, vars_to_collect)
 
+    def init_clean_file(self, clean_file):
+        self.dataClean = DataClean(clean_file)
+
     def process(self):
         if self.collector != None:
             self.collector.collect()
+        if self.dataClean != None:
+            self.dataClean.clean_data('./files_vars.csv')
 
      
 if __name__ == "__main__":
@@ -22,8 +29,12 @@ if __name__ == "__main__":
 
     bugs_to_collect = '-cb' in args
     vars_to_collect = '-cv' in args
+    clean_file = '-cf' in args
 
     if bugs_to_collect or vars_to_collect:
         main.init_collector(bugs_to_collect, vars_to_collect)
+
+    if clean_file:
+        main.init_clean_file(clean_file)
 
     main.process()
